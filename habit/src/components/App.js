2ff3347fe48406c80ee867f1620b20211f08e577
Ishika Thakur <ishikathakur0708@gmail.com>
2025-01-06 15:57:57 +0530

    Building Landing Page

---
+++ habit/src/components/App.js
@@ -0,0 +1,17 @@
+import logo from '../logo.svg';
+import '../components-css/App.css';
+import './Header';
+import './LandingPage';
+import LandingPage from "./LandingPage";
+import Header from "./Header";
+
+
+function App() {
+  return (
+    <div className="App">
+      <LandingPage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ habit/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import '../components-css/Header.css';
+
+const Header=()=>{
+    return(
+        <header className={'App-header'}>
+            <h1>habit.</h1>
+        </header>
+    );
+};
+export default Header;
