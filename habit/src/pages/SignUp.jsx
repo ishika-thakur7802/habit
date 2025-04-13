0b1eb31f4639c06773ceac5ab15ba2320d8575cb
Ishika Thakur <ishikathakur0708@gmail.com>
2025-04-13 23:52:42 +0530

    sign up event handlers

--- habit/src/pages/SignUp.jsx
+++ habit/src/pages/SignUp.jsx
@@ -2,10 +2,20 @@ import React from 'react';
 import '../pages-css/LandingPage.css';
 import Header from "../components/Header";
 import {Link} from "react-router-dom";
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-
-
+const handleSubmit=(e)=>
+{
+    e.preventDefault()
+    console.log(formData)
+}
+const Form=()=>{
+    const[formData, setFormData]=useState({
+        name:"",
+        email:"",
+        password:""
+    })
+}
 const SignUp = () => {
     return (
         <div className={'SignUp'}>
@@ -19,10 +29,10 @@ const SignUp = () => {
                         <h4>Build that Habit NOW!</h4>
                     </div>
                     <div className={'Ques-Fields'}>
-                        <form>
-                            <input type={"text"} placeholder={"Name"}/><br/>
-                            <input type={"text"} placeholder={"E-Mail"}/><br/>
-                            <input type={"password"} placeholder={"Password"}/><br/>
+                        <form onSubmit={handleSubmit}>
+                            <input onChange={(e) => setFormData({...formData, name: e.target.value})} value={formData.name} type={"text"} placeholder={"Name"} name={"name"} id={"name"}/><br/>
+                            <input onChange={(e)=>setFormData({...formData, email:e.target.value})} value={formData.email} type={"text"} placeholder={"E-Mail"} name={"email"} id={"email"}/><br/>
+                            <input onChange={(e)=>setFormData({...formData, password: e.target.value})} value={formData.password} type={"password"} placeholder={"Password"} name={"password"} id={"password"}/><br/>
                             <input type={"submit"} value={"LESGO"}/>
                         </form>
                     </div>
