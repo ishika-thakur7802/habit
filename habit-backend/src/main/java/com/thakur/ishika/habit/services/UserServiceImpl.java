e95a9b65fff39e4f8604eaf4246d742c931fd04f
Ishika Thakur <ishikathakur0708@gmail.com>
2025-05-08 09:42:31 +0530

    changed to constructor based injection instead of autowired in user service for code to be immutable, clean and maintainable plus better for testing

--- habit-backend/src/main/java/com/thakur/ishika/habit/services/UserServiceImpl.java
+++ habit-backend/src/main/java/com/thakur/ishika/habit/services/UserServiceImpl.java
@@ -10,8 +10,11 @@ import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService{
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @Override
     public List<User> findAll(){
